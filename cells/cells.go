229e529ccbf562ec382d3e2ac68f31f4c1c78d30
Eric Izoita <ericizoita@gmail.com>
2017-01-08 19:17:20 -0800

    Cells description

--- cells/cells.go
+++ cells/cells.go
@@ -1,6 +1,8 @@
+// Package cells provides an implementation agnostic interface for storage
+// and manipulation of cellular and synaptic state.
 package cells
 
 // Cells is an interface for TemporalMemory compatible cellular and synaptic
-// state storage.
+// state.
 type Cells interface {
 }
